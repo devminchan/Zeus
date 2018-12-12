3f55bef14d23c67d5487185530d8c457c3324c47
MinchanKim <syubsyubboy@gmail.com>
2018-12-12 10:23:15 +0900

    Configure project structure Add Services

--- app/src/main/java/com/example/minchan/zeus/view/MainActivity.java
+++ app/src/main/java/com/example/minchan/zeus/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.minchan.zeus;
+package com.example.minchan.zeus.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.minchan.zeus.R;
 
 public class MainActivity extends AppCompatActivity {
 
