d8ece4bcb7387fb10fc0a93ce5e95d88e83c6f9e
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2018-12-13 19:23:59 +0900

    [UPDATE} Google Sign In
    구글 로그인 기능 추가.

--- app/src/main/java/com/example/minchan/zeus/activity/IntroActivity.java
+++ app/src/main/java/com/example/minchan/zeus/activity/IntroActivity.java
@@ -3,11 +3,7 @@ package com.example.minchan.zeus.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.example.minchan.zeus.R;
