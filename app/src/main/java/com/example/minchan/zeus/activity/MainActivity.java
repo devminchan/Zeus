ce5d6661a02bea732d04a18de9ef6d22d20c8c40
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2018-12-14 10:49:01 +0900

    [DELETE] google login delete
    임시 삭제

--- app/src/main/java/com/example/minchan/zeus/activity/MainActivity.java
+++ app/src/main/java/com/example/minchan/zeus/activity/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        picture = (ImageView)findViewById(R.id.weathernow);
-        picture.setImageDrawable(getDrawable(R.drawable.snow));
+        //picture = (ImageView)findViewById(R.id.weathernow);
+        //picture.setImageDrawable(getDrawable(R.drawable.snow));
     }
 }
