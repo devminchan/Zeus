38da4eaa365e8df7fe559d75df54c576c46d0b7b
MinchanKim <syubsyubboy@gmail.com>
2018-12-17 08:56:40 +0900

    add fireService bind code

--- app/src/main/java/com/example/minchan/zeus/service/FireService.java
+++ app/src/main/java/com/example/minchan/zeus/service/FireService.java
@@ -1,14 +1,41 @@
 package com.example.minchan.zeus.service;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 public class FireService extends Service {
+
+    public class FireServiceBinder extends Binder {
+        IBinder getService() {
+            return FireServiceBinder.this;
+        }
+    }
+
+    private IBinder mBinder = new FireServiceBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flag, int startId) {
+        return super.onStartCommand(intent, flag, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
 // 이거도 마찬가지
