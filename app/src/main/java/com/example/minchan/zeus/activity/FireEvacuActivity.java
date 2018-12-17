a70c7707d2d32e3c807e9a0f34c73fe8449fb463
MinchanKim <syubsyubboy@gmail.com>
2018-12-17 10:08:16 +0900

    Set BroadCastReceiver between FireEvacuActivity and FireService

--- app/src/main/java/com/example/minchan/zeus/activity/FireEvacuActivity.java
+++ app/src/main/java/com/example/minchan/zeus/activity/FireEvacuActivity.java
@@ -1,5 +1,10 @@
 package com.example.minchan.zeus.activity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +12,31 @@ import com.example.minchan.zeus.R;
 
 public class FireEvacuActivity extends AppCompatActivity {
 
+    private BroadcastReceiver eventReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_evacu);
+
+        eventReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                float myAzimuth = intent.getFloatExtra("azimuth-me", 0f);
+            }
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(eventReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(eventReceiver, new IntentFilter("zeus:fire-event"));
     }
 }
--- app/src/main/java/com/example/minchan/zeus/service/FireService.java
+++ app/src/main/java/com/example/minchan/zeus/service/FireService.java
@@ -1,11 +1,17 @@
 package com.example.minchan.zeus.service;
+
 import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 
-public class FireService extends Service {
+public class FireService extends Service implements SensorEventListener {
 
     public class FireServiceBinder extends Binder {
         IBinder getService() {
@@ -15,15 +21,65 @@ public class FireService extends Service {
 
     private IBinder mBinder = new FireServiceBinder();
 
+    private SensorManager mSensorManager;
+    private Sensor mAccSensor;
+    private Sensor mMagSensor;
+
+    private float[] m_acc_data = null;
+    private float[] m_mag_data = null;
+
+    private float[] m_rotation = new float[9];
+    private float[] m_result_data = new float[9];
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, mMagSensor, SensorManager.SENSOR_DELAY_UI);
+
         return mBinder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccSensor = (Sensor) mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagSensor = (Sensor) mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            m_acc_data = event.values.clone();
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            m_mag_data = event.values.clone();
+        }
+
+        // 데이터가 존재하는 경우
+        if (m_acc_data != null && m_mag_data != null) {
+            // 가속 데이터와 자기장 데이터로 회전 매트릭스를 얻는다.
+            SensorManager.getRotationMatrix(m_rotation, null, m_acc_data, m_mag_data);
+            // 회전 매트릭스로 방향 데이터를 얻는다.
+            SensorManager.getOrientation(m_rotation, m_result_data);
+        }
+
+        Intent intent = new Intent("zeus:fire-event");
+        intent.putExtra("azimuth-me", getAzimuth(m_result_data[0]));
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+    private float getAzimuth(float read_azimuth) {
+        float azimuth = (float) Math.toDegrees(read_azimuth);
+
+        if (azimuth < 0)
+            azimuth += 360;
+
+        return azimuth;
     }
 
     @Override
@@ -33,9 +89,19 @@ public class FireService extends Service {
 
     @Override
     public void onDestroy() {
+        mSensorManager.unregisterListener(this);
         super.onDestroy();
     }
 
+    @Override
+    public boolean onUnbind(Intent intent) {
+        mSensorManager.unregisterListener(this);
+        return super.onUnbind(intent);
+    }
 
+    @Override
+    public void onRebind(Intent intent) {
+        mSensorManager.registerListener(this, mAccSensor, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, mMagSensor, SensorManager.SENSOR_DELAY_UI);
+    }
 }
-// 이거도 마찬가지
